d9c62dfef069fb7d058199a6f83268eaad276234
Mihai-Alexandru Ciungu <mciung18@student.aau.dk>
2020-05-08 13:57:05 +0200

    Final changes

--- waves.py
+++ waves.py
@@ -16,31 +16,31 @@ notes = {  # maps keyboard keys to musical notes
 def wave(note, duration=1):
     """ Base method for generating basic wave data that must then be transformed into specialized waveforms """
 
-    freq = notes[note]                             # Hz
-    sampling_rate = 44100                          # Hz
-    n_data = duration * sampling_rate
-    time = np.arange(0, n_data).T / sampling_rate  # s
-    init_phase = np.pi / 2                         # rad
+    frequency = notes[note]
+    samp_rate = 44100
+    n_data = duration * samp_rate
+    time = np.arange(0, n_data).T / samp_rate
+    init_phase = np.pi / 2
 
-    return freq, time, init_phase
+    return frequency, time, init_phase
 
 
 def sine(note, amplitude):
     """ Generates a sine wave at given note and with given amplitude """
 
-    freq, time, init_phase = wave(note)
-    return amplitude * np.cos(2 * np.pi * freq * time + init_phase)
+    frequency, time, init_phase = wave(note)
+    return amplitude * np.cos(2 * np.pi * frequency * time + init_phase)
 
 
 def square(note, amplitude):
     """ Generates a square wave at given note and with given amplitude """
 
-    freq, time, init_phase = wave(note)
-    return amplitude * signal.square(2 * np.pi * freq * time + init_phase, duty=0.5)
+    frequency, time, init_phase = wave(note)
+    return amplitude * signal.square(2 * np.pi * frequency * time + init_phase, duty=0.5)
 
 
 def saw(note, amplitude):
     """ Generates a saw wave at given note and with given amplitude """
 
-    freq, time, init_phase = wave(note)
-    return amplitude * signal.sawtooth(2 * np.pi * freq * time + init_phase)
+    frequency, time, init_phase = wave(note)
+    return amplitude * signal.sawtooth(2 * np.pi * frequency * time + init_phase)
